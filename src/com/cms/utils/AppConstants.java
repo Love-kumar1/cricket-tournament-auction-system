5eaf167c791b613515f2fee1ba55b77bd8c5e291
Love Kumar <lkamlhi99@gmail.com>
2026-07-02 11:40:08 +0500

    feat: add application-wide constants for roles and messages

---
+++ src/com/cms/utils/AppConstants.java
@@ -0,0 +1,13 @@
+package com.cms.utils;
+
+public class AppConstants {
+
+  public static final String LOGIN_DASHBOARD_MSG = "Login Successful!";
+  public static final String ADMIN_ROLE = "Admin";
+  public static final String MANAGER_ROLE = "Team manager";
+  public static final String PLAYER_ROLE = "Player";
+  public static final String WRONG_ROLE_MSG = "Wrong Role";
+  public static final String FAILED_LOGIN_MSG = "Login Failed! Invalid Email or Password.";
+
+
+}
